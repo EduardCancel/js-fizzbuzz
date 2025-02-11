1e51b173dbde4832f92057907c371bcd9c28fda4
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-11 14:39:59 +0100

    Complete output rule

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ let output;
 for (let i = 1; i <=100; i++){
     
     if ( i % 5 === 0 && i % 3 === 0){
-        output = "FizzBuzz"
-    } 
+
+        output = "FizzBuzz";
+
+    } else if (i % 3 === 0){
+
+        output = "Fizz";
+
+    } else if (i % 5 === 0){
+
+        output = "Buzz";
+
+    } else {
+        output = i;
+    }
+
+    console.log(`The number is ${output}`);
 }
