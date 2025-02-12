96ffbafd96e8fa6fa875a73125225540db4e59b6
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-12 09:35:03 +0100

    Fix space

--- main.js
+++ main.js
@@ -3,22 +3,14 @@
 
 let output;
 for (let i = 1; i <=100; i++){
-    
     if ( i % 5 === 0 && i % 3 === 0){
-
         output = "FizzBuzz";
-
     } else if (i % 3 === 0){
-
         output = "Fizz";
-
     } else if (i % 5 === 0){
-
         output = "Buzz";
-
     } else {
         output = i;
     }
-
     console.log(`The number is ${output}`);
 }
